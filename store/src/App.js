1fd65c7e51be0de5cc4469ba80e8751d58724109
ilkay <06ilkayozturk@gmail.com>
2023-10-24 15:37:51 +0300

    RESTAPI

--- store/src/App.js
+++ store/src/App.js
@@ -1,3 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import ProductsDetails from "./pages/ProductDetails";
+
 export default function App() {
-  return <h1>Hi Stafen!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products/:id" element={<ProductsDetails />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ store/src/pages/Products.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+export default function Products() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch("http://localhost:1337/api/products");
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error: {error.message} </h1>;
+
+  return (
+    <div>
+      <ul>
+        {data.data.map((product) => (
+          <li key={product.id}>
+            <h2>{product.attributes.name} </h2>
+            <code>{product.attributes.price} </code>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
